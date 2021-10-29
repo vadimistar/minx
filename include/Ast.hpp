461fa0af24b8c8d67154f1649c144b722511888c
Vadimistar <vadimistar@gmail.com>
2021-10-29 20:20:18 +0700

    moved ast to stack and separated parser from lexer

--- include/Ast.hpp
+++ include/Ast.hpp
@@ -6,102 +6,52 @@
 #define MINX_AST_HPP
 
 #include "Type.hpp"
-#include "Visitor.hpp"
-#include <memory>
-#include <optional>
-#include <string>
+
+#include <variant>
 #include <vector>
+#include <string>
+#include <optional>
 
 namespace minx {
 
-struct Node {
-  virtual ~Node() = default;
-  virtual void accept(Generator *) = 0;
-};
-
-struct Expr : Node {
-  ~Expr() override = default;
-  void accept(Generator *) override {
-    assert(0 && "Null expr occured");
-  }
-};
-struct Stmt : Node {
-  ~Stmt() override = default;
-  void accept(Generator *) override {}
-};
-struct Decl : Stmt {
-  ~Decl() override = default;
-  void accept(Generator *) override {}
-};
-
-using DeclVector = std::vector<std::unique_ptr<Decl>>;
-using StmtVector = std::vector<std::unique_ptr<Stmt>>;
-
-struct VarDecl : Decl {
+struct IdentifierAST {
   std::string name;
+};
+
+struct PrimitiveTypeAST; struct PointerTypeAST;
+
+using TypeAST = std::variant<PrimitiveTypeAST, PointerTypeAST>;
+
+struct PrimitiveTypeAST {
   PrimitiveType type;
-  std::optional<std::unique_ptr<Expr>> value;
-
-  ~VarDecl() override = default;
-
-  void accept(Generator *gen) noexcept override {
-    gen->append("%%%s = alloca %s\n", name.c_str(), getLLVMType(type).data());
-    if (value) {
-      gen->append("store ");
-      auto tmp { gen->expectedType };
-      gen->expectedType = type;
-      value.value()->accept(gen);
-      gen->expectedType = tmp;
-      gen->append(", %s* %%%s\n", getLLVMType(type).data(), name.c_str());
-    }
-  }
 };
 
-struct FuncDecl : Decl {
-  std::string name;
-  PrimitiveType type;
-  StmtVector body;
-
-  ~FuncDecl() override = default;
-
-  void accept(Generator *gen) noexcept override {
-    gen->append("define %s @%s() {\n", getLLVMType(type).data(), name.c_str());
-    gen->expectedType = type;
-    for (auto &i : body) {
-      i->accept(gen);
-    }
-    gen->expectedType = PrimitiveType::Null;
-    gen->append("}\n");
-  }
+struct PointerTypeAST {
+  TypeAST *dst;
 };
 
-struct IntLiteralExpr : Expr {
+struct IntegerLiteralAST {
   std::string value;
-
-  ~IntLiteralExpr() override = default;
-  void accept(Generator *gen) noexcept override {
-    gen->append("%s %s", getLLVMType(gen->expectedType).data(), value.c_str());
-  }
 };
 
-struct FloatLiteralExpr : Expr {
-  std::string value;
-
-  ~FloatLiteralExpr() override = default;
-  void accept(Generator *gen) noexcept override {
-    gen->append("%s %s", getLLVMType(gen->expectedType).data(), value.c_str());
-  }
+struct ReturnStmt {
+  std::optional<IntegerLiteralAST> value;
 };
 
-struct ReturnStmt : Stmt {
-  std::unique_ptr<Expr> value;
+using StatementAST = std::variant<std::monostate, ReturnStmt>;
 
-  ~ReturnStmt() override = default;
-  void accept(Generator *gen) noexcept override {
-    gen->append("ret ");
-    value->accept(gen);
-    gen->append("\n");
-  }
+struct CompoundStmt {
+  std::vector<StatementAST> statements;
+};
+
+struct FuncDeclAST {
+  IdentifierAST id;
+  TypeAST returnType;
+  CompoundStmt body;
+};
+
+struct TranslationUnitAST {
+  std::vector<FuncDeclAST> funcDecls;
 };
 
 } // namespace minx
