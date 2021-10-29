4faf2d83ebcbbb9f5c4e822121596e7dd02b692a
Vadimistar <vadimistar@gmail.com>
2021-10-29 20:16:26 +0700

    added locations to tokens

--- include/Token.hpp
+++ include/Token.hpp
@@ -5,18 +5,20 @@
 #ifndef MINX_TOKEN_HPP
 #define MINX_TOKEN_HPP
 
+#include "FileLocation.hpp"
 #include <string>
 
 namespace minx {
 
 enum struct TokenKind {
   Eof, Integer, Float, Word, Assign, Semicolon,
-  LParen, RParen, LBrace, RBrace, ThinArrow,
+  LParen, RParen, LBrace, RBrace, ThinArrow, Mul,
 };
 
 struct Token {
   TokenKind kind;
   std::string value;
+  FileLocation location;
 };
 
 std::string_view tokenKindAsStr(TokenKind t);
