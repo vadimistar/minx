b859b9c6a40fe047a6e13a6c5645d96bc10caed2
Vadimistar <vadimistar@gmail.com>
2021-10-29 20:18:53 +0700

    clean up code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "../include/Io.hpp"
 #include "../include/Lexer.hpp"
 #include <Parser.hpp>
+#include <cassert>
 
 int main(int argc, char **argv) {
   using namespace minx;
@@ -14,46 +15,46 @@ int main(int argc, char **argv) {
                  argv[0]);
   };
 
-  const auto error = [&printHelp] (const char* t_msg) {
+  const auto logFatal = [&printHelp] (const char *t_msg) {
     printHelp();
-    std::fprintf(stderr, "%s", t_msg);
+    std::fprintf(stderr, "Fatal: %s\n", t_msg);
     exit(1);
   };
 
   if (argc == 1) {
-    error("Error: No input files provided\n");
+    logFatal("Error: No input files provided");
   } else if (argc > 2) {
     assert(0 && "multiple files compilation is unavailable.");
   }
 
   if (!std::string_view{argv[1]}.ends_with(".minx")) {
     printHelp();
-    std::fprintf(stderr, "Error: %s: incorrect extension of the file (must be .minx)", argv[1]);
+    std::fprintf(stderr, "Fatal: %s: incorrect extension of the file (must be .minx)", argv[1]);
   }
   auto fileData{readFile(argv[1])};
   if (!fileData) {
     printHelp();
-    std::fprintf(stderr, "Error: Input file can't be opened\n");
-    exit(1);
+    logFatal("Input file can't be opened");
   }
   Lexer lexer(argv[1], fileData.value());
   Parser parser(lexer);
-  auto unit = parser.parseTranslationUnitDecl();
-  auto gen { std::make_unique<Generator>() };
-  for (auto &i: unit) {
-    i->accept(gen.get());
-  }
-  std::string newFileName { argv[1] };
+  auto unit = parser.parseTranslationUnit();
+
+  //auto gen { std::make_unique<LLVMGenerator>() };
+  //for (auto &i: unit) {
+  //  i->accept(gen.get());
+  //}
+  /*std::string newFileName { argv[1] };
   newFileName.erase(newFileName.size() - 5);
   constexpr auto maxBuff = 256u;
   char buff[maxBuff];
   sprintf(buff, "%s.ll", newFileName.c_str());
-  writeFile(buff, gen->data);
+  writeFile(buff, gen);
   sprintf(buff, "llvm-as %s.ll -o %s.bc; "
                 "llc -filetype=obj %s.bc; "
                 "gcc -no-pie %s.o -o %s;"
                 "rm %s.o; rm %s.bc", newFileName.c_str(),
           newFileName.c_str(), newFileName.c_str(), newFileName.c_str(),
           newFileName.c_str(), newFileName.c_str(), newFileName.c_str());
-  system(buff);
+  system(buff);*/
 }
