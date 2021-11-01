e3ff8cf3b040d8ed1aba18f463600560ac0a1bca
Vadimistar <vadimistar@gmail.com>
2021-11-01 18:02:24 +0700

    recreated scope struct

--- include/Scope.hpp
+++ include/Scope.hpp
@@ -5,6 +5,21 @@
 #ifndef MINX_SCOPE_HPP
 #define MINX_SCOPE_HPP
 
-class Scope {};
+#include "Ast.hpp"
+#include <map>
+
+namespace minx {
+
+struct Scope {
+  std::map<IdentifierAST, TypeAST> data;
+  Scope *prev;
+
+  explicit Scope(Scope *t_prev = nullptr) noexcept : prev(t_prev) {}
+
+  Scope &add(const IdentifierAST &t_id, const TypeAST &t_type) noexcept;
+  std::optional<TypeAST> getType(const IdentifierAST &t_id) noexcept;
+};
+
+}
 
 #endif // MINX_SCOPE_HPP
